77828ffc31a8a7843375f03cd42fa1e7b8ebee3b
angel <angelriera1796@gmail.com>
2022-05-15 20:03:38 -0400

    knows

--- src/components/onepage/Header.jsx
+++ src/components/onepage/Header.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 const Header = () => {
 
@@ -5,25 +6,32 @@ const Header = () => {
         <div className="mb-26 " id="about">
             <div className="
             bg-gradient-to-b from-teal-900 to-sky-900 bg-left-top bg-auto bg-repeat-x 
-            h-[22rem] md:h-[8cm] 
-            -mb-[17rem] md:-mb-[4cm] lg:-mb-[6cm]">
+            h-[22rem] md:h-[30rem] 
+            -mb-[17rem] md:-mb-[3cm] lg:-mb-[28rem]">
             </div>
             <div className='mx-auto md:m-24'>
-                <div className="my-5 text-center">
-                    <h1 className="text-[5rem] font-bold mb-5 md:mb-12 leading-[5rem]">Angel Riera</h1>
-                    <h2 className="text-[2rem] underline font-bold mb-10 leading-[2rem]">Full Stack Django React Web Developer</h2>
+                {/* <div className="flex justify-center h-auto w-32">
+                    <Image className="rounded-full" width={400} height={600} src={"/images/Angel_Riera.jpg"} />
+                </div> */}
+
+                <div className="my-5 text-center mb-10">
+                    <h1 className="text-[3.5rem] sm:text-[4rem] lg:text-[5rem] font-bold mb-5 md:mb-4 leading-[5rem]">Angel Riera</h1>
+                    <div>
+                        <h2 className="text-[2rem] md:text-[2rem] mb-5 font-bold  leading-[2rem]">Full Stack <span className="underline shadow-black font-extrabold text-teal-600">Django</span> <span className="underline shadow-black font-extrabold text-sky-600">React</span>  Web Developer</h2>
+                        <div className="mx-auto border-b w-1/2" />
+                    </div>
                 </div>
                 <div className="mb-12 text-center">
-                    <h3 className="text-[3rem] font-bold mb-10 underline leading-[2rem]">who am I?</h3>
-                    <div className="text-left mx-6 flex flex-col space-y-6">
-                        <p>
-                            - I am a full-stack web developer specialized in Django and React, <span className="underline font-semibold">with 2 years of experience in developing multiple web applications.</span>
+                    {/* <h3 className="text-[2rem] lg:text-[2rem] md:text-[3rem] font-bold mb-10 underline leading-[2rem]">who am I?</h3> */}
+                    <div className="text-center mx-6 flex flex-col space-y-6">
+                        <p className="underline font-semibold">
+                            I have 2 years of experience in developing multiple web applications
                         </p>
                         <p>
-                            - I am passionate about devising solutions of minimum effort and maximum result, self-sustainable and durable over time, for the problems I face or to improve my social environment following the design principles of permaculture adopted as a philosophy of life.
-                        </p>    
+                            I am passionate about devising solutions of minimum effort and maximum result, self-sustainable and durable over time, for the problems I face or to improve my social environment following the design principles of permaculture adopted as a philosophy of life.
+                        </p>
                     </div>
-                </div>            
+                </div>
             </div>
         </div>
     )
--- src/components/onepage/SkillsExp.jsx
+++ src/components/onepage/SkillsExp.jsx
@@ -12,56 +12,90 @@ const SkillsExp = () => {
         slider.current.scrollLeft += 50
     }, [50])
 
+    const sectors = [
+        "civil construction",
+        "agriculture",
+        "ecology",
+        "agroecology",
+        "education",
+        "agriculture",
+        "education",
+        "health",
+        "healthy lifestyle",
+        "physiotherapy",
+        "history",
+        "martial arts",
+        "digital marketing",
+        "SEO",
+        "copywriting",
+        "design"
+    ]
+
 
     return (
         <div className="mb-12 mx-auto md:m-24 text-center">
-            <h3 className="text-[3rem] font-bold mb-10 underline leading-[4rem] mx-4">Experience and Technical skills </h3>
-            <div className="mx-6 grid grid-cols-1 md:grid-cols-2 gap-[5rem] text-center">
+            <div className='mb-24'>
 
-                <div className='border py-10 px-8'>
-                    <Image width={100} height={100} src={`/images/exp01.jpg`} alt={`exp01`} className='object-cover object-center w-full' />
+                {/* <h3 className="text-[3rem] font-bold mb-10 underline leading-[4rem] mx-4">Experience</h3> */}
+                <div className="mx-6 grid grid-cols-1 md:grid-cols-2 gap-[5rem] text-center">
+
+                    <div className='border rounded-3xl py-16 px-8'>
+
+                        <Image width={100} height={100} src={`/images/exp01.jpg`} alt={`exp01`} className='object-cover object-center w-full' />
+                        <p className=''>
+                            I have experience in teamwork and collaborations with agile methodologies such as scrum, I tend to be one of the most capable within the groups of dev collaborators, because of my commitment to the work and my wide range of knowledge and preparation.
+                        </p>
+
+                    </div>
+
+                    <div className='border rounded-3xl pt-12 pb-8 px-8'>
+                        <Image width={100} height={100} src={`/images/exp02.jpg`} alt={`exp02`} className='object-cover object-center w-full' />
+                        <p className='underline'> I also know other sectors such as
+                        </p>
+                        <ul className='text-center grid grid-cols-2'>
+                            {sectors.map((e, i) => (
+                                <li key={i}>{e}</li>
+                            ))}
+
+                        </ul>
 
 
-                    <p className=''>
-                        - I have experience in teamwork and collaborations with agile methodologies such as scrum, I tend to be one of the most capable within the groups of dev collaborators, because of my commitment to the work and my wide range of knowledge and preparation.
-                    </p>
-                </div>
-
-                <div className='border py-10 px-8'>
-                    <Image width={100} height={100} src={`/images/exp02.jpg`} alt={`exp02`} className='object-cover object-center w-full' />
-
-
-                    <p className=''>
-                        - I also know other sectors such as; civil construction, agriculture, ecology, agroecology, education, health, healthy lifestyle, physiotherapy, philosophy, history, martial arts, digital marketing, SEO, copywriting and design.
-                    </p>
+                    </div>
                 </div>
             </div>
 
-            <div className='my-16'>
-                <div className='flex items-center justify-center w-full h-full '>
-                    <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft -= 400}>
-                        <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 19l-7-7 7-7'></path></svg>
-                    </button>
-                    <div ref={slider} class='overflow-hidden scroll-smooth h-full flex items-center justify-start'>
-                        {skills.map((e, i) => (
-                            <div key={i} className='snapt-start flex-shrink-0 mx-4 w-full md:w-1/3'>
+            <div className='mb-24'>
 
-                                <div className="flex justify-center">
-                                    <div className='bg-gray-400 p-5 rounded-full'>
-                                        <Image width={50} height={50} src={`/images/skills/${e.name}.svg`} alt={`images${i}`} className='object-cover object-center w-full' />
+                <h3 className="text-[3rem] font-bold mb-10 underline leading-[4rem] mx-4">Technical skills </h3>
+
+
+                <div className='my-16'>
+
+                    <div className='flex items-center justify-center w-full h-full '>
+                        <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft -= 400}>
+                            <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 19l-7-7 7-7'></path></svg>
+                        </button>
+                        <div ref={slider} class='overflow-hidden scroll-smooth h-full flex items-center justify-start'>
+                            {skills.map((e, i) => (
+                                <div key={i} className='snapt-start flex-shrink-0 mx-4 w-full md:w-1/3'>
+
+                                    <div className="flex justify-center">
+                                        <div className='bg-gray-400 p-5 rounded-full'>
+                                            <Image width={50} height={50} src={`/images/skills/${e.name}.svg`} alt={`images${i}`} className='object-cover object-center w-full' />
+                                        </div>
                                     </div>
+
+                                    <h5>{e.name}</h5>
+                                    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, enim et totam veritatis quam inventore deleniti. Porro, ab, tempora repellat similique iusto eveniet ut distinctio aliquid autem recusandae accusamus corporis.</p>
                                 </div>
-
-                                <h5>{e.name}</h5>
-                                <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, enim et totam veritatis quam inventore deleniti. Porro, ab, tempora repellat similique iusto eveniet ut distinctio aliquid autem recusandae accusamus corporis.</p>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
+                        <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft += 400}>
+                            <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5l7 7-7 7'></path></svg>
+                        </button>
                     </div>
-                    <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft += 400}>
-                        <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5l7 7-7 7'></path></svg>
-                    </button>
-                </div>
 
+                </div>
             </div>
         </div>
 
