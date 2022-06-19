2c42401191e85ec69d8a13a55ec38b430c03d35e
angel <angelriera1796@gmail.com>
2022-06-19 17:41:01 -0400

    full carrusel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,73 +3,38 @@ import Image from 'next/image'
 const Navbar = () => {
 
     const display = () => {
-        const navmenu = document.querySelector("#navmenu")
-        navmenu.classList.toggle("hidden")
+        const navmenu = document.querySelector('#navmenu')
+        navmenu.classList.toggle('hidden')
     }
+
+    const btns = [
+        { name: 'contact', link: "#" },
+        { name: 'about', link: "#" },
+        { name: 'services', link: "#" },
+    ]
     return (
-        <div className="absolute z-20">
-            <nav className="w-full fixed top-0 left-0 right-0  sm:block 
-                justify-center flex       
-                bg-gradient-to-t from-blue-600 to-sky-700">
+        <nav className='flex px-[10%] shadow-sm shadow-slate-600'>
+            <div>
+                <a className='hover:font-semibold' href='#about'> Angel7dev</a>
+            </div>
 
 
+            {/* items menu */}
+            <div id='navmenu'>
+            </div>
+            {/* items menu */}
 
-                <div className='flex flex-col sm:flex-row justify-center items-center sm:pb-0 pb-4 '>
+            {/* hidden buttom */}
 
-   
-                    <div className='mx-8 w-full sm:w-1/5 ml-8 text-center font-bold text-xl'>
-
-
-                        <a href="#about">
-                            <div className='w-full'>Angel Riera</div>
-                            <div className='w-full flex justify-center items-center '>
-                                <Image className='App-logo' src="/images/logo192.png" alt="" width={30} height={30} />
-                                <p>
-                                    FullStack
-                                </p>
-                                <Image className='fill-teal-500' src="/images/Django.svg" alt="" width={30} height={30} />
-
-                            </div>
-                        </a>
-                    </div>
-
-
-
-                    <div className="w-full text-center font-medium 
-    flex-grow sm:flex sm:justify-center items-center" id='navmenu'>
-                        <a href="#projects"
-                            className="nav-btn2">
-                            Projects
-                        </a>
-
-                        <a href="#skills"
-                            className="nav-btn2">
-                            Skills
-                        </a>
-                        <a href="#contacto"
-                            className="nav-btn2">
-                            Contact
-                        </a>
-                        <a href="/Cv_Angel_Riera.pdf" target="_blank"
-                            className="nav-btn2">
-                            ⇩CV
-                        </a>
-                    </div>
-                </div>
-                {/* hidden buttom */}
-
-                <div className="flex mt-4 items-start sm:hidden">
-                    <button onClick={() => display()} className="flex items-center px-3 py-2 
-                                border rounded border-sky-700 hover:text-white hover:border-white
-                                text-white ">
-                        <svg className="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <title>Menu</title>
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                        </svg>
-                    </button>
-                </div>
-            </nav>
-        </div>
+            <div >
+                <button onClick={() => display()} >
+                    <svg viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                        <title>Menu</title>
+                        <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+                    </svg>
+                </button>
+            </div>
+        </nav>
 
     )
 }
--- src/components/home/LayoutProjects.jsx
+++ src/components/home/LayoutProjects.jsx
@@ -6,7 +6,6 @@ const LayoutProjects = () => {
     const images = [...Array(5).keys()];
 
     const layout = useSelector(e => e.portfolio.Layout_projects)
-    console.log(layout)
     return (
         <div className="mx-[4rem] my-[10rem]">
             <div className="border-b mx-[5%] mb-16">
@@ -18,16 +17,16 @@ const LayoutProjects = () => {
             <div className='grid grid-cols-1 md:grid-cols-2'>
                 {layout.map((e, i) => (
                     <div key={i} className='border border-black group relative w-auto flex h-full items-center justify-center flex-col mx-auto rounded-3xl'>
-                        
+
                         <Image width={500} height={250} src={e.image} alt={`images${i}`} className="" />
                         <div className='bg-gray-800 bg-opacity-10 hover:bg-opacity-70 absolute w-full h-full p-6'>
                             <div className='opacity-0 hover:opacity-100 hover:brightness-100 flex h-full items-center justify-center '>
                                 <div className="flex justify-around space-x-[4rem]">
-                                        <a className="z-3 px-6 py-2 mx-1 bg-gray-900 text-gray-200 border text-xs rounded hover:bg-gray-800"
-                                            href={e.gitHub} target="__blank" >GitHub</a>
+                                    <a className="z-3 px-6 py-2 mx-1 bg-gray-900 text-gray-200 border text-xs rounded hover:bg-gray-800"
+                                        href={e.gitHub} target="__blank" >GitHub</a>
 
-                                        <a className="px-6 py-2 mx-1 bg-gray-300 text-gray-900 text-xs rounded hover:bg-gray-400"
-                                            href={e.live} target="__blank">Live</a>
+                                    <a className="px-6 py-2 mx-1 bg-gray-300 text-gray-900 text-xs rounded hover:bg-gray-400"
+                                        href={e.live} target="__blank">Live</a>
                                 </div>
                             </div>
                         </div>
