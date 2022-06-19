e1422b9bb2fed769a90c7f617aea9ab26bafc26a
angel <angelriera1796@gmail.com>
2022-06-19 17:40:55 -0400

    full carrusel

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,40 +1,41 @@
-import Image from "next/image"
+import { useState } from 'react'
 
 const Header = () => {
-
+    //trae imagenes aleatorias
+    const ramdon = () => `https://picsum.photos/id/${Math.floor(Math.random() * 100)}/500/500`
+    const images = [ramdon(), ramdon()]
+    const [current, setCurrent] = useState(0)
+    console.log(current)
+    const set_Current = (boolean) => {
+        if (boolean == true) { // next
+            if (current == images.length - 1) {
+                setCurrent(0)
+            } else {
+                setCurrent(current + 1)
+            }
+        } else if (boolean == false) { // previus
+            if (current == 0) {
+                setCurrent(images.length - 1)
+            } else {
+                setCurrent(current - 1)
+            }
+        }
+    }
     return (
-        <div className="mb-26 " id="about">
-            <div className="bg-gradient-to-b from-teal-900 to-sky-900 bg-left-top bg-auto bg-repeat-x 
-            h-[22rem] sm:h-[28rem] md:h-[30rem] 
-            -mb-[20rem] sm:-mb-[25rem] md:-mb-[27rem]">
-
-               
-            </div>
-            <div className='mx-auto lg:mx-24 my-16'>
-                {/* <div className="flex justify-center h-auto w-32">
-                    <Image className="rounded-full" width={400} height={600} src={"/images/Angel_Riera.jpg"} />
-                </div> */}
-
-                <div className="my-5 text-center mb-6 md:mb-10">
-                    <h1 className="text-[3rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[5rem] font-bold mb-5 md:mb-4 md:leading-[5rem]">Angel Riera</h1>
-                    <div>
-                        <h2 className="text-[1.5rem] sm:text-[2rem] md:text-[2rem] mb-5 font-bold  leading-[2rem]">Full Stack <span className="underline shadow-black font-extrabold text-teal-600">Django</span> <span className="underline shadow-black font-extrabold text-sky-600">React</span>  Web Developer</h2>
-                        <div className="mx-auto border-b w-1/2" />
-                    </div>
+        <div style={{ 'background-image': `url("${images[current]}")` }}
+            className={`h-[100vh] w-full 
+            bg-cover bg-center bg-no-repeat bg-fixed 
+            min-w-screen min-w-full static 
+            `}>
+            <div className='flex justify-between items-center bg-gray-800 bg-opacity-10  absolute w-full h-full '>
+                <button onClick={() => set_Current(false)} className='bg-slate-700 mx-4'>{"<"}</button>
+                <div className='text-center w-1/2'>
+                <h1 className='text-7xl'>title</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem culpa recusandae quasi tempore iure placeat cupiditate facilis aperiam quae a eligendi, porro doloribus numquam voluptatibus neque enim error non unde?</p>
                 </div>
-                <div className="md:mb-12 text-center">
-                    {/* <h3 className="text-[2rem] lg:text-[2rem] md:text-[3rem] font-bold mb-10 underline leading-[2rem]">who am I?</h3> */}
-                    <div className="text-center mx-6 flex flex-col space-y-6">
-                        <p className="underline font-semibold">
-                            I have 2 years of experience in developing multiple web applications
-                        </p>
-                        <p>
-                            I am passionate about devising solutions of minimum effort and maximum result, self-sustainable and durable over time, for the problems I face or to improve my social environment following the design principles of permaculture adopted as a philosophy of life.
-                        </p>
-                    </div>
-                </div>
+                <button onClick={() => set_Current(true)} className='bg-slate-700 mx-4'>{">"}</button>
             </div>
-        </div>
+        </div >
     )
 }
 
