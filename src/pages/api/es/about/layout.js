2b4073001c4d068d49d3a6696d9eab1d4650bb97
angel <angelriera1796@gmail.com>
2022-07-18 12:00:49 -0400

    for deploy

--- src/pages/api/es/about/layout.js
+++ src/pages/api/es/about/layout.js
@@ -6,27 +6,27 @@ export default function handler(req, res) {
     obj: [
       {
         "name": "Portfolio Art",
-        "live": "https://angel7dv.github.io/Layout/art-portfolio/",
+        "live": "https://RierAngel.github.io/Layout/art-portfolio/",
         "image": "/images/LayoutProjects/portfolio-art.png",
         "gif": "https://www.youtube.com/watch?v=r9Z_ZLnenTs",
 
       },
       {
         "name": "Tienda online de moda",
-        "live": "https://angel7dv.github.io/Layout/aismoda/",
+        "live": "https://RierAngel.github.io/Layout/aismoda/",
         "image": "/images/LayoutProjects/aismoda.png",     
         "gif": "https://www.youtube.com/watch?v=KP8cfJHBAlQ",
       },
       {
         "name": "Pagina de aterrizaje para aplicación móvil",
-        "live": "https://angel7dv.github.io/Layout/sellapp/",
+        "live": "https://RierAngel.github.io/Layout/sellapp/",
         "image": "/images/LayoutProjects/sellapp.png",
         "gif": "https://www.youtube.com/watch?v=qfT3PDJ6Swg",
 
       },
       {
         "name": "termoindustria",
-        "live": "https://angel7dv.github.io/Layout/termoindustria/",
+        "live": "https://RierAngel.github.io/Layout/termoindustria/",
         "image": "/images/LayoutProjects/termoindustria.png",
         "gif": "https://www.youtube.com/watch?v=r9Z_ZLnenTs",
 
